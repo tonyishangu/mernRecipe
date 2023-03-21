5b42e81992e301c611e0498a8fe146a6521b6635
tonyishangu <tonyishangu@gmail.com>
2023-03-21 17:15:37 +0300

    connect with mongo db

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://tonyishangu:Saige1aw!!@recipes.ajjkui1.mongodb.net/recipes?retryWrites=true&w=majority")
+
+
+app.listen(3001, () => {
+    console.log('server running');
+})

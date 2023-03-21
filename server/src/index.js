7fafb5df9821f09589fe27b6f7a423f961ce5300
tonyishangu <tonyishangu@gmail.com>
2023-03-21 18:15:19 +0300

    route for register

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { userRouter } from './routes/users.js'
 
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/auth', userRouter)
 
 mongoose.connect("mongodb+srv://tonyishangu:Saige1aw!!@recipes.ajjkui1.mongodb.net/recipes?retryWrites=true&w=majority")
 
---
+++ server/src/routes/users.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const router = express.Router()
+
+router.post('/register')
+router.post('/login')
+
+export { router as userRouter }
